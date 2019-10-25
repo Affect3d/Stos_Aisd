40234e072df4e764aac00c3b221e8b7d6ed539de
Affect3d <32643159+Affect3d@users.noreply.github.com>
2019-10-25 16:53:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -185,4 +185,50 @@ void PrzeniesStos(elem*& stos1, elem*& stos2)
     }
 }
 
+int main() {
+    elem* lista = NULL;
 
+    cout << "Dodawanie, usuwanie oraz czytanie elementów z listy" << endl; 
+    // insert(1, 1, lista);
+    // insert(2, 2, lista);
+    // insert(3, 3, lista);
+    // insert(4, 4, lista);
+    // remove(1, lista);
+    // cout << read(1, lista) << endl; // powinno być 2
+
+    cout << "Pobieranie rozmiaru listy" << endl;
+    // cout << size(lista) << endl; // powinno być 3
+
+    cout << "Wypisywanie elementów listy" << endl;
+    // cout << print(lista) << endl; // powinno być 2, 3, 4
+
+    cout << "Wypisywanie elementów listy w odwrotnej kolejności z użyciem stosu" << endl;
+    // cout << printWithReverseOrderWithStack(lista) << endl; // powinno być 4, 3, 2
+
+    cout << "Wypisywanie elementów listy w odwrotnej kolejności bez dodatkowych struktur" << endl;
+    // cout << printWithReverseOrder(lista) << endl; // powinno być 4, 3, 2
+
+    cout << "Usuwanie całej listy" << endl;
+    // elem* listaDoUsuniecia = NULL;
+    // insert(1, 1, listaDoUsuniecia);
+    // destroy(listaDoUsuniecia);
+    // cout << (listaDoUsuniecia == NULL ? "T" : "N") << endl; // powinno być T
+
+    cout << "Przeszukiwanie" << endl;
+    // elem* znalezionyElement = search(4, lista);
+    // cout << (znalezionyElement == NULL ? "T" : "N") << endl; // powinno być N
+    // znalezionyElement = search(5, lista);
+    // cout << (znalezionyElement == NULL ? "T" : "N") << endl; // powinno być T
+
+    cout << "Wielomiany" << endl;
+    elem_poly* ep1 = new elem_poly(5,2);
+    ep1->nast = new elem_poly(1,7);
+    ep1->nast->nast = new elem_poly(0,4);
+    elem_poly* ep2 = new elem_poly(2,-1);
+    // elem_poly* ep3 = polyadd(ep1, ep2);
+    // cout << ep3->expo << " " << expo->nast->expo << endl; // powinno być 5 2
+
+    cout << "Ulamki Fareya" << endl;
+    // elem_farey* ef = ulamek_fareya(4);
+    // cout << ef->dzielnik << " " << ef->nast->dzielnik << " " << ef->nast->nast->dzielnik << endl; // powinno być 1 4 3
+}
