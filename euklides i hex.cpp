89222a2950dd82ee7661a193868b30cdd7d613cb
Affect3d <32643159+Affect3d@users.noreply.github.com>
2020-01-07 18:19:02 +0100

    Create euklides i hex.cpp

---
+++ euklides i hex.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int gcd ( int x , int y )
+{
+    if ( y == 0 )
+        return x;
+    else if ( x >= y && y > 0)
+        return gcd ( y , x % y );
+    else return gcd ( y , x );        // if x < y then go ahead and switch them around.
+}
+
+string convertHex(int num, string ss)
+{
+
+    int x = num % 16;
+    char x_char = x+'0';
+
+    if (num ==0)
+    {
+        return ss;
+    }
+
+    switch (x)
+    {
+        case 10: ss = "A"+ ss; break;
+        case 11: ss = "B"+ ss; break;
+        case 12: ss = "C"+ ss; break;
+        case 13: ss = "D"+ ss; break;
+        case 14: ss = "E"+ ss; break;
+        case 15: ss = "F"+ ss; break;
+        default: ss = x_char + ss; break;
+    }
+
+    return convertHex(num / 16, ss);
+
+}
+
+int main()
+{
+    //cout<< gcd(256, 280) << endl;
+    string buffer="";
+    cout << convertHex(177777, buffer)<<endl;
+    return 0;
+}
